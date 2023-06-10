d64cc0bc00909be9c8c9db1a786caa9de610fc43
jsm00 <jsd014@naver.com>
2023-06-10 16:40:53 +0900

    feature:resume_test

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -1,5 +1,8 @@
 const Resume = () => {
-  return <div>나는 프로젝트 내역</div>;
+  return <div><h1>실전코딩 수강</h1>
+    <p>2023.03 ~ 2023.06</p>
+    <p>Git, React, SpringBoot 등 다양한 실무 경험 학습</p>
+  </div>;
 };
 
 export default Resume;
